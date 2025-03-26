5a67bdb7394325765c7ae9ad338d77417f78a004
angelayandrofski <angelashan084@gmail.com>
2025-03-25 22:42:03 -0700

    test for cleaning data - not done yet

---
+++ R/clean_data.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(readr)
+library(dplyr)
+library(docopt)
+
+#cleaning a dataset 
+
+clean_animal_shelter_data <- function(df) {
+    df <- df %>% 
+        select(animal_type, primary_color, sex, dob, intake_condition, intake_type, intake_date, outcome_date, outcome_type) %>% 
+        #filter(animal_type != "other") %>%
+        na.omit()
+
+    df$outcome_group <- recode(df$outcome_type,
+                             "adoption" = "Adopted",
+                             "foster" = "Adopted",
+                             "foster to adopt" = "Adopted",
+                             "homefirst" = "Adopted",
+                             "rescue" = "Adopted",
+                             "euthanasia" = "Not Adopted",
+                             .default = "Not Adopted")
+  
+df$age_at_intake <- as.numeric(difftime(Sys.Date(), df$dob, units = "days")) / 365
+
+return(df)
+}
+
+main <- function(input_file_path, output_file_path) {
+  animals <- read_csv(input_file_path)
+  cleaned_animals <- clean_animals_data(animals)
+  write_csv(cleaned_animals, output_file_path)
+}
---
+++ tests/test_clean_data.R
@@ -0,0 +1,43 @@
+library(testthat)
+
+set.seed(2) 
+
+#test the clean data 
+source(here::here("R", "clean_data.R"))
+
+test_df <- tibble(
+    animal_type = c("dog", "cat", "reptile", "bird"),
+    primary_color = c("black", "brown", "white", "green"),
+    sex = c("Female", "Male", "Neutered", "Spayed"),
+    dob = as.Date(c("2013-02-18", "2021-03-15", "2024-10-10","2023-08-16")),
+    intake_condition = c("ill mild", "normal", "injured severe", "ill moderate"),
+    intake_type = c("stray", "wildlife", "owner surrender", "confiscate"),
+    intake_date = as.Date(c("2023-06-01", "2023-09-01", "2024-08-04", "2021-03-07")),
+    outcome_date = as.Date(c( "2023-07-01", "2023-10-01", "2024-05-27", "2022-05-19")),
+    outcome_type = c("adoption", "foster", "euthanasia", "rescue")
+  )
+
+cleaned_df <- clean_animal_shelter_data(test_df)
+
+test_that("clean_animal_shelter_data correctly removes NA values", {
+  expect_false(any(is.na(cleaned_df)))
+})
+
+test_that("clean_animal_shelter_data correctly recategorizes outcome types", {
+  expect_equal(cleaned_df$outcome_group[1], "Adopted")
+  expect_equal(cleaned_df$outcome_group[2], "Adopted")
+  expect_equal(cleaned_df$outcome_type[3], "Not Adopted")
+})
+
+
+test_that("clean_animal_shelter_data calculates age_at_intake correctly", {
+  expected_age <- as.numeric(difftime(test_df$intake_date[-3], test_df$dob[-3], units = "days")) / 365
+  expect_equal(cleaned_df$age_at_intake, expected_age, tolerance = 0.1)
+})
+
+test_that("clean_animal_shelter_data returns correct columns", {
+  expected_columns <- c("animal_type", "primary_color", "sex", "dob", "intake_condition", 
+                        "intake_type", "intake_date", "outcome_date", "outcome_type", 
+                        "outcome_group", "age_at_intake")
+  expect_equal(colnames(cleaned_df), expected_columns)
+})
