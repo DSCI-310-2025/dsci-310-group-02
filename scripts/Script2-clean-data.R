3418fb91eb53b76794516a456ab1f0c369b3b3b8
Nolan Vibhakar <mnvibhakar@gmail.com>
2025-04-07 21:28:40 -0700

    fixed script-2

--- scripts/Script2-clean-data.R
+++ scripts/Script2-clean-data.R
@@ -20,24 +20,8 @@ main <- function(input_file_path, output_file_path) {
   # read in data
   animals <- read_csv(input_file_path)
   
-  animals <- animals %>% 
-    select(animal_type, primary_color, sex, dob, intake_condition, intake_type, intake_date, outcome_date, outcome_type) %>% 
-    #filter(animal_type != "other") %>%
-    na.omit()
-  
-  animals$outcome_group <- recode(animals$outcome_type,
-                                  "adoption" = "Adopted",
-                                  "foster" = "Adopted",
-                                  "foster to adopt" = "Adopted",
-                                  "homefirst" = "Adopted",
-                                  "rescue" = "Adopted",
-                                  .default = "Not Adopted"  # Everything else is considered "Not Adopted"
-  )
-  
-  animals$age_at_intake <- as.numeric(difftime(Sys.Date(), animals$dob, units = "days")) / 365
-  
-  # Write cleaned data to output file
-   write_csv(animals, output_file_path)
+  # clean data using script
+  animals <- clean_animal_shelter_data(animals_) 
 }
 
 main(opt$input_file_path, opt$output_file_path)
